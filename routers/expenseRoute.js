cf6b74ce7ec1bb2df60e916562db1dd6d1ecd5d9
Puneet0910 <nainwalpuneet@gmail.com>
2024-11-21 16:38:46 +0530

    Daily expense app with userId

--- routers/expenseRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const expenseController = require("../controllers/expenseController");
-
-router.post("/add",expenseController.addExpense);
-router.get("/get",expenseController.getExpenses);
-router.delete("/delete/:id",expenseController.deleteExpense);
-module.exports = router;
---
+++ routes/expenseRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const expenseController = require('../controllers/expenseController');
+
+router.post('/add', expenseController.addExpense);
+router.get('/get', expenseController.getExpense);
+router.delete('/delete/:expenseId', expenseController.deleteExpense);
+
+module.exports = router;
